0d91145fb432370116b8ee6a863da09c48bf86b0
渡邉華輝 <harukiwatanabe@watanabehanaakiranoMacBook-Pro.local>
2023-10-24 23:27:58 +0900

    変数の名前の修正

--- kalama.ino
+++ kalama.ino
@@ -5,16 +5,13 @@ USBMIDI_CREATE_DEFAULT_INSTANCE();
 
 Nunchuk nchuk;
 
-int i = 0;
 //連打対策のため変数の数でMIDIのOn Offを切り替える
 long scale = 0;
 int pich = 0;
-int q = 0;
-int w = 0;
+int zButtonPressedLastCycle = 0;
+int cButtonPressedLastCycle = 0;
 int def = 60;
-int s = 0;
-int scale_memory = 0;
-
+int previous_scale = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -44,23 +41,23 @@ void loop() {
     //＋１２
 
     Serial.print("The Z button is ");
-    if (q == 0 && zButton == true) {
+    if (zButtonPressedLastCycle == 0 && zButton == true) {
       Serial.println("pressed");
       pich -= 12;
-      q = 1;
+      zButtonPressedLastCycle = 1;
     } else if (zButton == false) {
       Serial.println("released");
-      q = 0;
+      zButtonPressedLastCycle = 0;
     }
 
     Serial.print("The c button is ");
-    if (w == 0 && cButton == true) {
+    if (cButtonPressedLastCycle == 0 && cButton == true) {
       Serial.println("pressed");
       pich += 12;
-      w = 1;
+      cButtonPressedLastCycle = 1;
     } else if (cButton == false) {
       Serial.println("released");
-      w = 0;
+      cButtonPressedLastCycle = 0;
     }
 
 
@@ -81,13 +78,14 @@ void loop() {
     Serial.println(diff_Y);
 
     //tan(y/x)
-    double tan = atan2(diff_X,diff_Y);
+    double tan = atan2(diff_X, diff_Y);
     Serial.print("The tanjent is at ");
     Serial.println(tan);
 
-    int r = diff_X*diff_X + diff_Y*diff_Y;
+    int r = diff_X * diff_X + diff_Y * diff_Y;
     Serial.print("The range is at ");
     Serial.println(r);
+
 
     //delay(100);
 
@@ -100,55 +98,71 @@ void loop() {
     */
     //range_x = 250 (-125 -> 125) ,range_y = 250 (-125 -> 125)
 
-    if(r <_ 8800) return 0;
-
+    int rr = 6000;
 
     //joystick
-    if ((diff_X >= 0 && diff_Y >= 0 && tan <= 0.3839) || (diff_X <= 0 && diff_Y >= 0 && tan >= -0.3839)) {
-      scale = 0; //328-22 ド
-    } else if (diff_X >= 0 && diff_Y >= 0 && tan <= 1.1693 && tan >= 0.4014) {
-      scale = 2; //23-67　レ
-    } else if ((diff_X >= 0 && diff_Y >= 0 && tan >= 1.1868) || (diff_X >= 0 && diff_Y <= 0 && tan <= 1.9547)) {
-      scale = 4; //68-112　ミ
-    } else if (diff_X >= 0 && diff_Y <= 0 && tan >= 1.9722 && tan <= 2.7401){
-      scale = 5; //113-157　ファ
-    } else if ((diff_X >= 0 && diff_Y <= 0 && tan >= 2.7576) ||  (diff_X >= 0 && diff_Y <= 0 && tan <= -2.7576)){
-      scale = 7; //158-202　ソ
-    } else if (diff_X <= 0 && diff_Y <= 0 && tan >= -2.7401 && tan <= -1.9722){
-      scale = 9; //203-247　ラ
-    } else if ((diff_X <= 0 && diff_Y <= 0 && tan >= -1.9547) ||  (diff_X <= 0 && diff_Y >= 0 && tan <= -1.1868)){
-      scale = 11;
-    } else if (diff_X <= 0 && diff_Y >= 0 && tan >= -1.1693 && tan <= -0.4014){
-      scale = 12;
-    } else {
-      scale = 20;
+    if(r <= rr){
+      scale = -1;
+    }else{
+      scale = stick_radian_scale(diff_X,diff_Y);
     }
-    scale_memory = def + scale + pich;
-    //もし演奏時に違和感を感じたら、iをそれぞれ音に合わせてint d,r,m,f,...とすると良い
-    if (scale != 20 && i == 0) {
-      MIDI.sendNoteOn(def + scale + pich, 127, 1);  //ド
-      i = 1;
-      s = def + scale + pich;
 
-    } else if (scale == 20 && i == 1) {
-      MIDI.sendNoteOff(s, 127, 1);
-      i = 0;
+
+    int now_scale = def + scale + pich;
+
+    //もし演奏時に違和感を感じたら、iをそれぞれ音に合わせてint d,r,m,f,...とすると良い
+    if (scale == -1) {
+      MIDI.sendControlChange(midi::AllNotesOff, 0, 1);
+    } else if (now_scale != previous_scale) {
+      MIDI.sendNoteOff(previous_scale, 127, 1);
+      MIDI.sendNoteOn(now_scale, 127, 1);  //ド
     }
+
+    previous_scale = now_scale;
 
     //accel 511
     //-8192 ~ 8191
 
     // Read an accelerometer and print values (0-1023, X, Y, and Z)
-		int accelZ = nchuk.accelZ();
+    int accelZ = nchuk.accelZ();
     int adjust_accelZ = (double)(accelZ - 511) / 1024 * 16384;
 
-		//Serial.print("The accelerometer's X-axis is at ");
-		Serial.println(accelZ);
+    //Serial.print("The accelerometer's X-axis is at ");
+    Serial.println(accelZ);
 
-    MIDI.sendPitchBend(adjust_accelZ,1);
-    Serial.println(s);
+    MIDI.sendPitchBend(adjust_accelZ, 1);
+    //Serial.println(scale_memory);
     //delay(1000);
     // Print all the values!
     // nchuk.printDebug();
   }
+}
+
+double stick_radian_scale(double diff_X, double diff_Y) {
+  //tan(y/x)
+    double tan = atan2(diff_X, diff_Y);
+    Serial.print("The tanjent is at ");
+    Serial.println(tan);
+  //joystick
+  int scale;
+  if ((diff_X >= 0 && diff_Y >= 0 && tan <= 0.4014) || (diff_X <= 0 && diff_Y >= 0 && tan > -0.4014)) {
+    scale = 0;  //328-23 ド
+  } else if (diff_X >= 0 && diff_Y >= 0 && tan <= 1.1693 && tan > 0.4014) {
+    scale = 2;  //23-67　レ
+  } else if ((diff_X >= 0 && diff_Y >= 0 && tan > 1.1693) || (diff_X >= 0 && diff_Y <= 0 && tan <= 1.9547)) {
+    scale = 4;  //67-112　ミ
+  } else if (diff_X >= 0 && diff_Y <= 0 && tan > 1.9547 && tan <= 2.7401) {
+    scale = 5;  //112-157　ファ
+  } else if ((diff_X >= 0 && diff_Y <= 0 && tan > 2.7401) || (diff_X <= 0 && diff_Y <= 0 && tan <= -2.7576)) {
+    scale = 7;  //157-202　ソ
+  } else if (diff_X <= 0 && diff_Y <= 0 && tan > -2.7576 && tan <= -1.9722) {
+    scale = 9;  //202-247　ラ
+  } else if ((diff_X <= 0 && diff_Y <= 0 && tan > -1.9722) || (diff_X <= 0 && diff_Y >= 0 && tan <= -1.1868)) {
+    scale = 11;
+  } else if (diff_X <= 0 && diff_Y >= 0 && tan > -1.1868 && tan <= -0.4014) {
+    scale = 12;
+  } else {
+    scale = -1;
+  }
+  return scale;
 }
